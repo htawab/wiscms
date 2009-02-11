50853fdaf24a2f199bcca10b7306e8fb7b921370
327556231@qq.com <327556231@qq.com@8e4209f4-f1d8-11dd-ac65-f74f249cfaac>
2009-02-11 07:36:45 +0000

    改了部分css

--- trunk/wiscms/Wis.Toolkit/WebControls/DropdownMenus/DropdownMenu.cs
+++ trunk/wiscms/Wis.Toolkit/WebControls/DropdownMenus/DropdownMenu.cs
@@ -161,16 +161,16 @@ namespace Wis.Toolkit.WebControls.DropdownMenus
             string styleKey = string.Format("DropdownMenuCss_{0}", base.ClientID);
             string styleBlock = string.Format(@"
             <style type='text/css'>
-                ul.{0} {{list-style:none; margin:0; padding:0; width:200px; overflow:visible;}}
+                ul.{0} {{list-style:none; margin:0; padding:0; width:200px; overflow:visible; line-height:23px;}}
                 ul.{0} * {{margin:0; padding:0; cursor: pointer;}}
-                ul.{0} a {{display:block; color:#000; text-decoration:none;}}
+                ul.{0} a {{display:block; color:#000; text-decoration:none; height:22px;}}
                 ul.{0} li {{background:url({1}header.gif); position:relative; float:left; margin-right:2px; overflow:visible;}}
-                ul.{0} ul {{position:absolute; top:26px; left:0; background:#d1d1d1;  display:none; *opacity:0; list-style:none;}}
+                ul.{0} ul {{position:absolute; top:23px; left:0; background:#d1d1d1;  display:none; *opacity:0; list-style:none;}}
                 ul.{0} ul li {{position:relative; border:1px solid #aaa; width:167px; border-top:none;  margin:0}}
-                ul.{0} ul li a {{display:block; padding:3px 7px 5px; background-color:#d1d1d1}}
+                ul.{0} ul li a {{display:block; padding:0px 7px; height:22px; background-color:#d1d1d1}}
                 ul.{0} ul li a:hover {{background-color:#c5c5c5}}
-                ul.{0} ul ul {{left:167px; top:-1px}}
-                ul.{0} .menulink {{display:block; border:1px solid #aaa; padding:5px 15px 6px 7px; font-weight:bold; background:url({1}arrow3.gif) 152px 8px no-repeat; width:145px}}
+                ul.{0} ul ul {{left:168px; top:0px}}
+                ul.{0} .menulink {{display:block; border:1px solid #aaa; padding:0px 15px 0px 7px; font-weight:bold; background:url({1}arrow3.gif) 152px 8px no-repeat; width:145px}}
                 ul.{0} .menulink:hover, ul.menu .menuhover {{background:url({1}arrow2.gif) 152px 8px no-repeat;}}
                 ul.{0} .sub {{background:#d1d1d1 url({1}arrow.gif) 147px 8px no-repeat}}
                 ul.{0} .topline {{border-top:1px solid #aaa}}
